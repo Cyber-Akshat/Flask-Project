1a75e38d018e74708d1438484e9d73833ad804ce
AK$AT <S2502940@ndonline.ac.uk>
2026-06-15 10:29:50 +0100

    Designed the html website

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
-import sqlite3
 
 app = Flask(__name__)
 
-#app
-@app.route('/')
-def hello_world():  # put application's code here
+@app.route("/")
+def home():
     return render_template("home.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
