30fb0ff1884cc90f0f900933eae532c2c71f5d71
AK$AT <S2502940@ndonline.ac.uk>
2026-06-15 10:08:24 +0100

    Rendering the template in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import sqlite3
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template("home.html")
 
 
 if __name__ == '__main__':
